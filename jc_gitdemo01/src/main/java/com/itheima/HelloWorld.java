a63c39b3ac1dedef057f442283d2e435afc39d12
WooZieMu <1325647945@qq.com>
2020-07-04 13:34:17 +0800

    添加一个helloWorld类

--- jc_gitdemo01/src/main/java/com/itheima/HelloWorld.java
+++ jc_gitdemo01/src/main/java/com/itheima/HelloWorld.java
@@ -7,5 +7,6 @@ package com.itheima;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("i will find you,i swiss");
     }
 }
