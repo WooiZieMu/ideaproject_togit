feab0d78ce7343adce1f6841fc6b1c097f2df614
WooZieMu <1325647945@qq.com>
2020-07-04 13:32:06 +0800

    添加一个helloWorld类

---
+++ jc_gitdemo01/src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author : WoozieMu
+ * @date : 2020/07/04 13:29
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
